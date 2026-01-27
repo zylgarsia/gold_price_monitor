083fde276ad0c0d0b1dbce8edb42c7db3666faca
zyl <2845124186@qq.com>
2026-01-27 20:15:16 +0800

    delete config

--- gold_notice.py
+++ gold_notice.py
@@ -1,10 +1,16 @@
 import sys
 sys.stdout.reconfigure(encoding='utf-8')
-# notifier.py
+import os
 import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
-from config import *
+
+SMTP_SERVER = os.environ["SMTP_SERVER"]
+SMTP_PORT = int(os.environ["SMTP_PORT"])
+EMAIL_SENDER = os.environ["EMAIL_SENDER"]
+EMAIL_PASSWORD = os.environ["EMAIL_PASSWORD"]
+EMAIL_RECEIVER = os.environ["EMAIL_RECEIVER"]
+
 
 def send_email(subject, content):
     msg = MIMEText(content, "plain", "utf-8")
